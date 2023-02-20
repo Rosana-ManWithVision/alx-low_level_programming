6fb3a7f8608abd70a33b37fb877d35af072e7ecc
Rosana <rozanaezekiel@gmail.com>
2023-02-19 19:37:55 -0800

    6-second trail

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 /**
- * main - A program that print various sizes of various types on the computer it is running on
- * return o
+ * main - A program that print various sizes of various types
+ * main(void) - Return nothinig
+ * Return 0 - (Success)
  */
-int main (void)
+int main(void)
+{
 	char a;
 	int b;
-	long int c;long long intd;
+	long int c;
+	long long int d;
 	float f;
-	printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof (a));
-	printf("Size of an int: %lu byte(s)
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", sizeof(b));
+	printf("Size of long long int: %lld byte(s)\n", sizeof(d));
+	printf("Size of long int: %ld byte(s)\n", sizeof(c));
+	printf("Size of float  int: %f byte(s)\n", sizeof(f));
+	return (0); /* Returns nothing*/
+}
