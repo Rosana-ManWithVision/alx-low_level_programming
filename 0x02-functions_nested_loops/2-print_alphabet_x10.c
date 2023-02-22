79206de8ef7dd1c84ac8284f89c18003e57dae78
Rosana <rozanaezekiel@gmail.com>
2023-02-21 17:55:45 -0800

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet
+ */
+
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char letter;
+
+	while (count__ <= 9)
+	{
+		for (letter = 'a'; letter <= 'z' | ; letter++)
+			_putchar(letter);
+		_putchar('/n');
+	}
+}
+
