cfd22fc91243e41e38098be75957299595170b76
Rosana <rozanaezekiel@gmail.com>
2023-02-22 21:38:41 -0800

    tasks 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+
+include "main.h"
+/**
+ * _abs - compute int value
+ * @n: int in questions
+ * Return: the integer's abs
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
