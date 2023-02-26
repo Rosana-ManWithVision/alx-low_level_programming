a20167aa15046e305e79844cba880a71a38b3f98
Rosana <rozanaezekiel@gmail.com>
2023-02-26 13:31:00 -0800

    num 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - print the sign of a num
  * @n: the num to be checked for
- * Return: 1 and prints + if n is >0
+ * Retuirn: 1 and prints + if n is >0
  * 0 if n>0
  * -1 and prints - if n<o
  */
@@ -16,13 +16,4 @@ int print_sign(int n)
 		return (1);
 	}
 	else if (n == 0)
-	{
-		_putchar('0')
-		return (0);
 	}
-	else
-	{
-		_putchar('-');
-		return (-1);
-	}
-}
